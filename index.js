b20744fc6743f59fc7e1156b5073710ee3db83f2
JS <csdnum0@gmail.com>
2023-01-05 22:22:31 +0900

    D

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const PORT = 5010;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    let data = 0;
-    for (let i = 0; i < (1024 * 1024 * 1024 * 4); i++) {
-        data += i
-    }
-    res.status(200).send(`<h2>hello ${data}</h2>`);
-});
-
 app.get('/test/:count', (req, res) => {
     const count = req.params.count;
     let data = 0;
